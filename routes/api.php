bb3518f8f3656cd67d834b57c5b7ee468bc54ccd
Alex Pestell <alex.pestell@gmail.com>
2020-10-06 22:49:39 +0200

    Remove unused route

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::middleware('auth:sanctum')->get('book', [BookController::class, 'index']);
 Route::post('token', [AuthController::class, 'requestToken']);
